e9c91ac3997edd3474419eeb064bd76486cd78b1
Steven Miyakawa <steven.miyakawa@here.com>
2016-07-24 12:33:08 -0700

    Style Schedule component

--- src/js/components/Schedule.js
+++ src/js/components/Schedule.js
@@ -1,8 +1,10 @@
+import './Schedule.scss';
+
 import React, { Component, PropTypes } from 'react';
 import _ from 'lodash';
 import DaySchedule from './DaySchedule';
-import TimeColumn from './TimeColumn';
-import StageColumn from './StageColumn';
+
+import legend from '../../images/legend.png';
 
 export default class Schedule extends Component {
   static propTypes = {
@@ -13,18 +15,18 @@ export default class Schedule extends Component {
     const { schedule } = this.props;
     const groupedSchedules = _.groupBy(schedule, 'day');
 
-    return <div>
-      <h1>Schedule</h1>
-      <div>
-        <a href='#'>Friday</a>
-        <a href='#'>Saturday</a>
-        <a href='#'>Sunday</a>
+    return (
+      <div className="schedule-container">
+        <div className="m-b-2">
+          <a className="m-a-1" href='#'>Friday, August 5</a>
+          <a className="m-a-1" href='#'>Saturday, August 6</a>
+          <a className="m-a-1" href='#'>Sunday, August 7</a>
+        </div>
+        <img className="legend m-b-1" src={legend} alt="legend"/>
+        <DaySchedule day={'Friday'} schedule={groupedSchedules['2016-08-05']}/>
+        <DaySchedule day={'Saturday'} schedule={groupedSchedules['2016-08-06']}/>
+        <DaySchedule day={'Sunday'} schedule={groupedSchedules['2016-08-07']}/>
       </div>
-      <TimeColumn times={[]} />
-      <StageColumn />
-      <DaySchedule day={'Friday'} schedule={groupedSchedules['2016-08-05']}/>
-      <DaySchedule day={'Saturday'} schedule={groupedSchedules['2016-08-06']}/>
-      <DaySchedule day={'Sunday'} schedule={groupedSchedules['2016-08-07']}/>
-    </div>;
+    );
   }
 }
