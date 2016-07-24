e6452388b2a3336c742516132f10de0416c28366
Steven Miyakawa <steven.miyakawa@here.com>
2016-07-24 14:02:41 -0700

    Style StageColumn

--- src/js/components/StageColumn.js
+++ src/js/components/StageColumn.js
@@ -1,47 +1,20 @@
+import './StageColumn.scss';
+
 import React, { Component, PropTypes } from 'react';
 import Event from './Event';
 
 export default class StageColumn extends Component {
   render() {
-    const events = [
-    {  
-      tag:"recommended",
-      name:"lcd soundsystem",
-      stage:"Lands End",
-      day:"2016-08-05",
-      time:{  
-        start:"2010",
-        end:"2155"
-      }
-    },
-    {  
-      tag:"recommended",
-      name:"skeelo",
-      stage:"Lands End",
-      day:"2016-08-05",
-      time:{  
-        start:"2010",
-        end:"2155"
-      }
-    },
-    {  
-      tag:"recommended",
-      name:"weezer",
-      stage:"Lands End",
-      day:"2016-08-05",
-      time:{  
-        start:"2010",
-        end:"2155"
-      }
-    }
-    ];
+    const { name, events } = this.props;
     const eventsToRender = events.map((ev) => {
       return <Event event={ev} />;
     });
 
-    return <div>
-      <p>Stage Column</p>
-      {eventsToRender}
-    </div>;
+    return (
+      <div className="stage-column">
+        <p>{ name }</p>
+        {eventsToRender}
+      </div>
+    );
   }
 }
