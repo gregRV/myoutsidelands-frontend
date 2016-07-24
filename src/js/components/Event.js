706ee5ad037bd931db1548bd991fe8750a1a76fd
Greg Varias <gregvarias@gmail.com>
2016-07-24 14:35:12 -0700

    Add styles for 'Liked' and 'Recommended' Events

--- src/js/components/Event.js
+++ src/js/components/Event.js
@@ -1,15 +1,25 @@
+import './Event.scss';
+
 import React, { Component, PropTypes } from 'react';
+import heart from '../../images/heart.svg';
 import star from '../../images/star.svg';
 
 export default class Event extends Component {
   render() {
     const {event} = this.props;
-    const style = {
-
+    const recommendedStyle = {
+      backgroundColor: '#0F2043',
+      color: '#FFF'
     }
+    const likedStyle = {
+      backgroundColor: '#EF3B46',
+      color: '#FFF'
+    }
+    const style = event.tag === 'recommended' ? recommendedStyle : likedStyle;
+    const icon = event.tag === 'recommended' ? star : heart;
 
     return <div style={style}>
-      <img src={star} />
+      <img src={icon} />
       <p>{event.name}</p>
       <p>{event.time.start} - {event.time.end}</p>
     </div>;
