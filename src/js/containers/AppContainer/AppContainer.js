bdbd145e7699d10be3e26712b4d0573a1e05d506
Steven Miyakawa <steven.miyakawa@here.com>
2016-07-24 09:24:42 -0700

    Splash page MVP

--- src/js/containers/AppContainer/AppContainer.js
+++ src/js/containers/AppContainer/AppContainer.js
@@ -1,3 +1,10 @@
+import './AppContainer.scss';
+import yellowDenimBackground from '../../../images/yellow-yellower.jpg';
+import redDenimBackground from '../../../images/red-denim.jpg';
+import headerImg from '../../../images/header-img.png';
+import spotifyLogo from '../../../images/spotifyLogo-BLACK.svg';
+import connectNowButton from '../../../images/connectNow-btn.png';
+
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
@@ -11,8 +18,13 @@ export class AppContainer extends Component {
 
   render () {
     return (
-      <div>
-        <h1>Splash Page</h1>
+      <div className="app-container" style={{ backgroundImage: `url(${yellowDenimBackground})` }}>
+        <header className="text-sm-center" style={{ backgroundImage: `url(${redDenimBackground})` }}>
+          <img className="main-logo" src={headerImg} alt="main logo"/>
+        </header>
+        <img className="spotify-logo m-t-3 m-b-2" src={spotifyLogo} alt="spotify logo"/>
+        <p className="app-instructions">Link your Spotify accout to auto-generate a lineup schedule.</p>
+        <img className="connect-now-button m-t-2" src={connectNowButton} alt="connect now button"/>
       </div>
     );
   }
