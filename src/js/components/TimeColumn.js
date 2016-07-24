544d82a55870c32f4b3b11aae9ea2bb35666333d
Greg Varias <gregvarias@gmail.com>
2016-07-24 12:41:01 -0700

    TimeColumn renders all times in 15 minute increments

--- src/js/components/TimeColumn.js
+++ src/js/components/TimeColumn.js
@@ -1,15 +1,31 @@
 import React, { Component, PropTypes } from 'react';
 
 export default class TimeColumn extends Component {
-  static propTypes = {
-    times: PropTypes.array.isRequired
-  };
+  // static propTypes = {
+  //   times: PropTypes.array.isRequired
+  // };
 
   render() {
-    const {times} = this.props;
+    // const {times} = this.props;
+    // for each hour
+      // return a list of that hour joined with elements from Minute list
+    const hours = ['12', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'];
+    const minutes = ['00', '15', '30', '45']
+    const times = _.flatten(hours.map((hour) => {
+      return minutes.map((min) => {
+        return `${hour}:${min}`;
+      });
+    }));
+    console.log('times:', times);
+    const timesToRender = times.map((ts, i) => {
+      return <li key={i}>{ts}</li>;
+    });
 
     return <div>
-      <p>Time Column</p>
+      <h1>Times</h1>
+      <ul>
+        {timesToRender}
+      </ul>
     </div>;
   }
 }
